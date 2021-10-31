d36b49b03228365dc5f1dfeb9c0e196850038ac8
Nevoral <NevoralTomas@gmail.com>
2021-10-31 19:53:31 +0100

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from web import create_app
-
+#from web import create_app
+from flask import Flask
+app =  Flask(__name__)
+@app.route('/')
+def home():
+    return 'Hello World!'
 if __name__ == '__main__':
-    app = create_app()
+    #app = create_app()
     app.run()
